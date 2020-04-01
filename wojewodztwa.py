a36f231ba591aefae3b65247a9321f3083e43914
Alicja Ciuńczyk <41646139+AlicjaCiunczyk@users.noreply.github.com>
2020-04-01 16:36:28 +0200

    Add population density

--- wojewodztwa.py
+++ wojewodztwa.py
@@ -17,18 +17,24 @@ map_df['JPT_NAZWA_'] = ['Śląskie', 'Opolskie', 'Wielkopolskie', 'Zachodniopomo
 map_df.plot()
 
 # Import the csv file with the population data
-pop = pd.read_csv('Population.csv', sep=';')
+pop = pd.read_csv("Population.csv", sep=";")
 pop.head()
 
 # Merge the two dataframes together
-merged = pd.merge(map_df, pop, on = 'JPT_NAZWA_')
+merged = pd.merge(map_df, pop, on = "JPT_NAZWA_")
+# Create density column
+merged["Density"] = merged["Population"]/merged["Area"]
 
+### POPULATION PER VOIVODESHIP ###
 # create figure and axes for Matplotlib
 fig, ax = plt.subplots(figsize = (16, 12))
 merged["rep"] = merged["geometry"].centroid
 map_points = merged.copy()
 # Plot the heatmap of population
-map_points.plot(column = merged["Population"], cmap = "Greens", linewidth = 1.2, ax = ax, edgecolor = "0.7")
+map_points.plot(column = merged["Population"], cmap = "Greens", linewidth = 1.2,
+                ax = ax, edgecolor = "0.7", legend=True, 
+                legend_kwds={'label': "Population by Voivodeship", 
+                             'orientation': "horizontal"})
 map_points.set_geometry("rep", inplace = True)
 
 # Add Voivodeship names
@@ -37,5 +43,22 @@ for i in range(len(merged["rep"])):
     y = merged["rep"][i].bounds[1]
     label = merged["JPT_NAZWA_"][i]
     ax.text(x, y, label, fontsize = 10)
-fig
+
+### POPULATION DENSITY PER VOIVODESHIP ###
+# create figure and axes for Matplotlib
+fig1, ax1 = plt.subplots(figsize = (16, 12))
+map_points = merged.copy()
+# Plot the heatmap of population density
+map_points.plot(column = merged["Density"], cmap = "Reds", linewidth = 1.2, ax = ax1,
+                edgecolor = "0.7", legend=True, 
+                legend_kwds={'label': "Population density by Voivodeship", 
+                             'orientation': "horizontal"})
+map_points.set_geometry("rep", inplace = True)
+
+# Add Voivodeship names
+for i in range(len(merged["rep"])):
+    x = merged["rep"][i].bounds[0]
+    y = merged["rep"][i].bounds[1]
+    label = merged["JPT_NAZWA_"][i]
+    ax1.text(x, y, label, fontsize = 10)
 
