53f8cfc1d7cabf36278e946f1cf5a01302339e1c
Alicja Ciuńczyk <41646139+AlicjaCiunczyk@users.noreply.github.com>
2020-04-01 16:08:01 +0200

    Add files via upload

--- wojewodztwa.py
+++ wojewodztwa.py
@@ -21,4 +21,21 @@ pop = pd.read_csv('Population.csv', sep=';')
 pop.head()
 
 # Merge the two dataframes together
-merged = pd.merge(map_df, pop, 'JPT_NAZWA_')
+merged = pd.merge(map_df, pop, on = 'JPT_NAZWA_')
+
+# create figure and axes for Matplotlib
+fig, ax = plt.subplots(figsize = (16, 12))
+merged["rep"] = merged["geometry"].centroid
+map_points = merged.copy()
+# Plot the heatmap of population
+map_points.plot(column = merged["Population"], cmap = "Greens", linewidth = 1.2, ax = ax, edgecolor = "0.7")
+map_points.set_geometry("rep", inplace = True)
+
+# Add Voivodeship names
+for i in range(len(merged["rep"])):
+    x = merged["rep"][i].bounds[0]
+    y = merged["rep"][i].bounds[1]
+    label = merged["JPT_NAZWA_"][i]
+    ax.text(x, y, label, fontsize = 10)
+fig
+
